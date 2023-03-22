0fa7c0389b724d41c96595191d4bc51aba9a85ca
basilmt <57865169+basilmt@users.noreply.github.com>
2023-03-22 11:23:53 +0000

    added cart context

--- myapp/src/components/UI/Input.js
+++ myapp/src/components/UI/Input.js
@@ -1,10 +1,12 @@
+import React from 'react'
+
 import classes from './Input.module.css'
 
-const Input = props => {
-    return <div className={classes.input}>
+const Input = React.forwardRef((props, ref) => {
+    return (<div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}></input>
-    </div>
-}
+        <input ref={ref} {...props.input}></input>
+    </div>);
+});
 
 export default Input
